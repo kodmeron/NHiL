20af7c7d13a9df2e543702361deaef9c17b1da33
JonathanBacklin <Jonathan.backlin@gmail.com>
2022-09-01 00:17:13 +0200

    Added: Hello World Fetch From Backend

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import './App.css';
 import "leaflet/dist/leaflet.css"
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Icon } from "leaflet";
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [fetchData, setFetchData] = useState({})
+
+  const fetchDataFunction = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/text')
+      const responseData = await response.json()
+      setFetchData(responseData)
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+  useEffect(() => {
+    fetchDataFunction()
+  }, [])
   return (
     <div className='leaflet-container'>
+      {fetchData ? <h1>{fetchData.message}</h1> : null}
       <MapContainer center={[59.32, 18.07]} zoom={14} scrollWheelZoom={false}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
