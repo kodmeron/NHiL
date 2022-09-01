8d615d7d27fc7a44c25587bf8f535ec426bb3bb9
kodmeron <89447350+kodmeron@users.noreply.github.com>
2022-09-01 20:50:15 +0200

    Hello world from backend

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from "react"
 import './App.css';
 import "leaflet/dist/leaflet.css"
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from "leaflet";
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [fetchData, setFetchData] = useState({})
 
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
+  const fetchDataFunction = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/text')
+      const responseData = await response.json()
+      setFetchData(responseData)
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+  useEffect(() => {
+    fetchDataFunction()
+  }, [])
   return (
     <div className='leaflet-container'>
       {fetchData ? <h1>{fetchData.message}</h1> : null}
@@ -22,7 +29,6 @@ function App() {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
       </MapContainer>
-      <p style={{ color: '#000000' }}>{!data ? "Laddar..." : data}</p>
     </div>
   );
 }
