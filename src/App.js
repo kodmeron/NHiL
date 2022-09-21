0333b8954d11c60e044710c929f3b76619c9ca58
Eliaz Ravello <eliaz@telia.com>
2022-09-21 10:51:45 +0200

    fix: can now pull

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import './App.css';
+import "./App.css";
 
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 import { L } from "leaflet";
-import { useEffect, useState, useRef } from 'react';
-import { logIn, logOut, signUp, useAuth, db } from './firebase';
-import { async } from '@firebase/util';
-import { onSnapshot, collection, getDocs, addDoc } from "@firebase/firestore"
-import markerIcon from "./images/marker-icon.png"
-import { Link } from 'react-router-dom'
-import Navbar from './Components/Navbar';
+import { useEffect, useState, useRef } from "react";
+import { logIn, logOut, signUp, useAuth, db } from "./firebase";
+import { async } from "@firebase/util";
+import { onSnapshot, collection, getDocs, addDoc } from "@firebase/firestore";
+import markerIcon from "./images/marker-icon.png";
+import { Link } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  const currentUser = useAuth()
+  const currentUser = useAuth();
 
   // FIRESTORE READ
   const [locations, setLocations] = useState([]);
@@ -19,11 +25,11 @@ function App() {
 
   const getLocations = async () => {
     const data = await getDocs(locationsCollectionRef);
-    setLocations(data.docs.map((doc) => ({ ...doc.data() })))
+    setLocations(data.docs.map((doc) => ({ ...doc.data() })));
   };
 
   useEffect(() => {
-    getLocations()
+    getLocations();
   }, []);
 
   // FIRESTORE CREATE
@@ -33,143 +39,157 @@ function App() {
   const [newLongitude, setNewLongitude] = useState("");
 
   const createPin = async () => {
-    await addDoc(locationsCollectionRef, { lat: newLatitude, long: newLongitude, place: newLocationName });
-  }
+    await addDoc(locationsCollectionRef, {
+      lat: newLatitude,
+      long: newLongitude,
+      place: newLocationName,
+    });
+  };
 
   // CREATE USER!
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-  const emailRef = useRef()
-  const passwordRef = useRef()
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
   const handleSignUp = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      await signUp(emailRef.current.value, passwordRef.current.value)
+      await signUp(emailRef.current.value, passwordRef.current.value);
     } catch (error) {
-      alert('Invalid Input')
+      alert("Invalid Input");
     }
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
   // LOG IN USER
 
   const handleLogIn = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      await logIn(emailRef.current.value, passwordRef.current.value)
+      await logIn(emailRef.current.value, passwordRef.current.value);
     } catch (error) {
-      alert('Error!')
+      alert("Error!");
     }
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
   const handleLogOut = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      await logOut()
+      await logOut();
     } catch (error) {
-      alert('Could not login! Try again')
+      alert("Could not login! Try again");
     }
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
   // SET PIN AT LOCATION
 
   const LocationMarker = () => {
-    const [position, setPosition] = useState(null)
+    const [position, setPosition] = useState(null);
     const map = useMapEvents({
       click() {
-        map.locate()
+        map.locate();
       },
       locationfound(e) {
-        setPosition(e.latlng)
-        map.flyTo(e.latlng, map.getZoom())
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
       },
-    })
+    });
 
     return position === null ? null : (
-      <Marker position={position} >
+      <Marker position={position}>
         <Popup>You did it here</Popup>
       </Marker>
-    )
-  }
+    );
+  };
 
   return (
     <div>
       <Navbar currentUser={currentUser} handleLogOut={handleLogOut} />
-      <div className='leaflet-container'>
+      <div className="leaflet-container">
         <MapContainer center={[59.32, 18.07]} zoom={14} scrollWheelZoom={false}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
           <LocationMarker />
-
           {locations.map((location) => {
-            return <Marker position={[location.lat, location.long]}></Marker>
-          })
-
-          };
-
-
-
+            return <Marker position={[location.lat, location.long]}></Marker>;
+          })}
+          ;
         </MapContainer>
       </div>
 
-
       <div>
-        {currentUser ? <>Currently logged in as: {currentUser?.email} </> : null}
+        {currentUser ? (
+          <>Currently logged in as: {currentUser?.email} </>
+        ) : null}
       </div>
 
-      {!currentUser ? <>
-        <div className='Signup-form'>
-          <input ref={emailRef} placeholder='Email' />
-          <input ref={passwordRef} type='password' placeholder='Password' />
+      {
+        !currentUser ? (
+          <>
+            <div className="Signup-form">
+              <input ref={emailRef} placeholder="Email" />
+              <input ref={passwordRef} type="password" placeholder="Password" />
 
-          <div className='Signup-buttons'>
-            <button disabled={loading || currentUser} onClick={handleSignUp}>Sign Up</button>
-            <button disabled={loading || currentUser} onClick={handleLogIn}>Log In</button>
-          </div>
-        </div>
-      </>
-        :
-        null
+              <div className="Signup-buttons">
+                <button
+                  disabled={loading || currentUser}
+                  onClick={handleSignUp}
+                >
+                  Sign Up
+                </button>
+                <button disabled={loading || currentUser} onClick={handleLogIn}>
+                  Log In
+                </button>
+              </div>
+            </div>
+          </>
+        ) : null
         // <button disabled={loading || !currentUser} onClick={handleLogOut}>Log Out</button>
       }
 
       <div>
-        {currentUser ? <>
-          <input placeholder='Name'
-            onChange={(event) => {
-              setNewLocationName(event.target.value);
-            }}
-          />
-          <input placeholder='Latitude'
-            onChange={(event) => {
-              setNewLatitude(event.target.value);
-            }}
-          />
-          <input placeholder='Longitude'
-            onChange={(event) => {
-              setNewLongitude(event.target.value);
-            }}
-          />
-          <button onClick={createPin}>Set new pin</button>
-        </>
-          : null}
+        {currentUser ? (
+          <>
+            <input
+              placeholder="Name"
+              onChange={(event) => {
+                setNewLocationName(event.target.value);
+              }}
+            />
+            <input
+              placeholder="Latitude"
+              onChange={(event) => {
+                setNewLatitude(event.target.value);
+              }}
+            />
+            <input
+              placeholder="Longitude"
+              onChange={(event) => {
+                setNewLongitude(event.target.value);
+              }}
+            />
+            <button onClick={createPin}>Set new pin</button>
+          </>
+        ) : null}
       </div>
 
-      <div style={{ textAlign: 'center' }}>
+      <div style={{ textAlign: "center" }}>
         <h1>
-          {currentUser ? <>You can now do everything on the website</> :
+          {currentUser ? (
+            <>You can now do everything on the website</>
+          ) : (
             <>You must login to get access</>
-          }
+          )}
         </h1>
       </div>
     </div>
   );
 }
 
-export default App; 
+export default App;
